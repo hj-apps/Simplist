f3dd04499b1485709cb27630bebfa9c49f40ef47
Jonathan Yeap <jonathanyeap+1@gmail.com>
2020-02-29 14:02:36 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Table, Button} from 'reactstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faThumbsUp, faThumbsDown, faImage, faMoneyCheckAlt, faSearchDollar} from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = { isLoading: false, items : [
+
+                {
+                    "id" :"100",
+                    "Title":"sunglasses",
+                    "Quantity":"2",
+                    "Importance":"Nice to Have"
+                },
+                {
+                    "id" :"200",
+                    "Title":"passports",
+                    "Quantity":"2",
+                    "Importance":"Must Have"
+                },
+                {
+                    "id" :"300",
+                    "Title":"wallets",
+                    "Quantity":"2",
+                    "Importance":"Must Have"
+                }
+            ]
+    }
+
+    removeItem(id){
+        console.log(id)
+        let updatedItems = [...this.state.items].filter(i => i.id !== id)
+        this.setState ({items : updatedItems});
+    }
+
+    //todo: setState
+    render() {
+        const isLoading = this.state.isLoading;
+        const allItems = this.state.items;
+
+            if (isLoading)
+                return (<div>Loading....</div>)
+
+        let items =
+        allItems.map( item =>
+            <tr key={item.id}>
+                <td>{item.Quantity}</td>
+                <td>{item.Title}</td>
+                <td>{item.Importance}</td>
+                <td><Button className="btn btn-lg btn-success" onClick={() => this.removeItem(item.id)}>    <FontAwesomeIcon icon={faThumbsUp} /> DONE</Button></td>
+            </tr>
+        ) 
+            
+        return (
+            <div className="container border border-secondary rounded center">
+
+                <div className="row">
+                    <div className="col-12">
+                        <h4>Pending Items - HJ's Items</h4>
+                    </div>
+                </div>
+                
+                <div className="row">
+                    <div className=".col-sx-12 center text-center">
+                        <Table dark responsive striped bordered hover>
+                            <thead>
+                                <tr>
+                                <th scope="row">Quantity</th>
+                                <th>Title</th>
+                                <th>Importance</th>
+                                <th colSpan="1">Actions</th>
+                                </tr>
+                            </thead>
+
+                        <tbody>
+                            {this.state.items.length === 0 ? <td colSpan="5">All caught up!</td> : items}
+                        </tbody>
+                        </Table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
